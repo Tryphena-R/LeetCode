c1b461cf56f8d4e532675fdd2eb5dcf7668289e6
Tryphena-R <165980003+Tryphena-R@users.noreply.github.com>
2026-06-15 09:13:25 +0530

    Time: 12 ms (13.4%), Space: 46.4 MB (95.06%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,18 +1,18 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        for (ListNode head : lists) {
-            while (head != null) {
+        ArrayList<Integer> arr= new ArrayList<>();
+        for(ListNode head: lists) {
+            while(head != null) {
                 arr.add(head.val);
                 head = head.next;
             }
         }
         Collections.sort(arr);
-        ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
-        for (int num : arr) {
-            temp.next = new ListNode(num);
-            temp = temp.next;
+        ListNode dummy= new ListNode();
+        ListNode temp= dummy;
+        for (int num:arr) {
+            temp.next= new ListNode(num);
+            temp= temp.next;
         }
         return dummy.next;
     }
